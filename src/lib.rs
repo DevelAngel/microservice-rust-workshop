c396d66f4ba8edae399d55d173f90b1964c34105
Angelos Drossos <develangel@mail.drossos.de>
2022-11-03 12:37:15 +0100

    feat: impl lesson-1

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ use std::{
 
 use axum::{body::Bytes, Router};
 
+use axum::response::{IntoResponse, Html};
+use axum::routing::get;
+use axum::extract::Query;
+
 /// Custom type for a shared state
 pub type SharedState = Arc<RwLock<AppState>>;
 #[derive(Default)]
@@ -12,6 +16,28 @@ pub struct AppState {
     db: HashMap<String, Bytes>,
 }
 
-pub fn router(_state: &SharedState) -> Router<SharedState> {
-    todo!()
+pub fn router(state: &SharedState) -> Router<SharedState> {
+    Router::with_state(state.clone())
+        .route("/", get(hello_world))
+        //.route("/hello", get(say_hi_unknown))
+        .route("/hello", get(say_hi_stefan))
+}
+
+async fn hello_world() -> impl IntoResponse {
+    "<h1>Hello Axum</h1>"
+}
+
+#[allow(dead_code)]
+async fn say_hi_unknown() -> impl IntoResponse {
+    Html("<h1>Hello Unknown Visitor</h1>")
+}
+
+#[allow(dead_code)]
+async fn say_hi_stefan(Query(param): Query<HashMap<String, String>>) -> impl IntoResponse {
+    let name = if let Some(name) = param.get("name") {
+        name
+    } else {
+        "Unknown Visitor"
+    };
+    Html(format!("<h1>Hello {name}</h1>"))
 }
